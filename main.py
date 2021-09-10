48fbfe9b86258b689e25675bed95bf270c0f611b
Nabeel Ansari <80488887+nabeel-ds@users.noreply.github.com>
2021-09-10 18:24:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-# Configure your home page.
+# Configuring the home page.
 st.set_page_config(page_title = 'Car Price Prediction',
                     page_icon = ':car:',
                     layout = 'centered',
